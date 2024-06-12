e39e1cd058392ed7cadcd94139e81f0584689d8e
Brenda Urquiza <burquiza@jla.local>
2024-06-12 16:51:40 -0300

    Commit

--- assets/components/tarjetasPortfolio.js
+++ assets/components/tarjetasPortfolio.js
@@ -6,22 +6,28 @@ document.addEventListener('DOMContentLoaded', () => {
         portfolioContainer.innerHTML = ''; // Limpiar el contenedor antes de añadir nuevas tarjetas
         proyectos.forEach(proyecto => {
             const nuevoProyecto = document.createElement("div");
-            nuevoProyecto.classList = "tarjeta lg:w-1/4 md:w-1/2 p-4 w-full"; // Asegúrate de que la clase 'tarjeta' se añade aquí
+            nuevoProyecto.classList = "tarjeta lg:w-3/4 md:w-3/4 p-4 w-full mt-8"; // Asegúrate de que la clase 'tarjeta' se añade aquí
             nuevoProyecto.innerHTML = `
-                <a class="block relative h-48 rounded overflow-hidden">
-                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="${proyecto.imagen}">
-                </a>
-                <div class="mt-4">
+            <div class="flex sm:-m-4 -mx-4 -mb-10 -mt-4">
+                <div class="rounded-lg h-48 w-96 overflow-hidden">
+                    <img alt="content" class="object-cover object-center h-full w-full" src="${proyecto.imagen}">
+                </div>
+                <div class="p-4 md:w-3/5 sm:mb-0 mb-6">
+                    <h2 class="text-xl font-medium title-font text-gray-200 mt-5">${proyecto.titulo}</h2>
                     <h3 class="text-gray-400 text-xs tracking-widest title-font mb-1">${proyecto.categoria}</h3>
-                    <h2 class="text-gray-100 title-font text-lg font-medium">${proyecto.titulo}</h2>
+                    <p class="text-base leading-relaxed mt-2">${proyecto.description}</p>
+                    <div class="buttons flex flex-wrap">
+                        ${proyecto.links ? proyecto.links.map(link => `<a href="${link.href}"><button class="btnPortfolio mr-2"><i class='${link.icon}' style='color:#ffffff'></i></button></a>`).join('') : `<a href="${proyecto.link}"><button class="btnPortfolio mr-2"><i class='${proyecto.icon}' style='color:#ffffff'></i></button></a>`}
+                    </div>
                 </div>
-                <div class="buttons flex flex-wrap">
-                    ${proyecto.links ? proyecto.links.map(link => `<a href="${link.href}"><button class="btnPortfolio mr-2"><i class='${link.icon}' style='color:#ffffff'></i></button></a>`).join('') : `<a href="${proyecto.link}"><button class="btnPortfolio mr-2"><i class='${proyecto.icon}' style='color:#ffffff'></i></button></a>`}
-                </div>
+             </div>   
             `;
             portfolioContainer.appendChild(nuevoProyecto);
         });
     }
+
+
+    
 
     function filtrarTarjetasPorCategoria(categoria) {
         const tarjetas = document.querySelectorAll('.tarjeta');
